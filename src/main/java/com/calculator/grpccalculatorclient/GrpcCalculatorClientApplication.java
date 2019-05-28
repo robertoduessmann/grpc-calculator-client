9e7cea6c66a49505225dd1baf4af651737d3ac81
Roberto Duessmann <roberto.duessmann@gmail.com>
2019-05-28 20:55:10 +0200

    Project bootstrap

---
+++ src/main/java/com/calculator/grpccalculatorclient/GrpcCalculatorClientApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.grpccalculatorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcCalculatorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcCalculatorClientApplication.class, args);
+	}
+
+}
